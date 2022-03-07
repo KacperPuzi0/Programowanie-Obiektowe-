0d17e549ba7fafaeb07477ddeab4d959fa52b4fd
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-03-07 19:20:31 +0100

    Pierwsze ćw

--- Lab1 - 02.03/Program.cs
+++ Lab1 - 02.03/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        PersonProperties PersonProperties = PersonProperties.of("Kacperek");
+        Console.WriteLine(PersonProperties.FirstName);
+    }
+}
+public class PersonProperties
+{
+    private string _firstName;
+
+    private PersonProperties(string firstName)
+    {
+        _firstName = firstName;
+    }
+
+    public static PersonProperties of(string firstName)
+    {
+        if (firstName.Length >= 2)
+        {
+            return new PersonProperties(firstName);
+        }
+        else
+        {
+            throw new ArgumentException("Nazwa jest za krotka");
+        }
+    }
+    public string FirstName
+    {
+        get
+        {
+            return _firstName;
+        }
+        set
+        {
+            if (value.Length >= 3)
+            {
+                _firstName = value;
+            }
+        }
+    }
+}
