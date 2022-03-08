12e4fc3a76778f03d70a43c4e66121d0dae40f43
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-03-08 14:09:11 +0100

    ++

--- Lab1 - 02.03/Program.cs
+++ Lab1 - 02.03/Program.cs
@@ -40,4 +40,60 @@ public class PersonProperties
             }
         }
     }
+
+    // Klasa Money 
+    public enum Currency
+    {
+        PLN = 1,
+        USD = 2,
+        EUR = 3
+    }
+    public class Money
+    {
+        private readonly decimal _value;
+        private readonly Currency _currency;
+        private Money(decimal value, Currency currency)
+        {
+            _value = value;
+            _currency = currency;   
+        }
+
+        public static Money? Of(decimal value, Currency currency)
+        {
+            return value < 0 ? null : new Money(value, currency);
+        }
+
+        // Ćwiczenie 1 
+        public static Money? OfWithException(decimal value, Currency currency)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Hubert nie masz pieniędzy na koncie");
+            }
+            else
+            {
+                return new Money(value, currency);
+            }
+        }
+
+        // Ćwiczenie 2 
+        public static Money? ParseValue(string value, Currency currency)
+        {
+            decimal parseValue;
+            bool succes = decimal.TryParse(value, out parseValue);
+            if (succes)
+            {
+                return Money.Of(parseValue, currency);
+            }
+            else
+            {
+                throw new ArgumentException("Niepoprawny argument");
+            }
+        }
+
+        // Ćwiczenie 3 
+
+
+    }
+
 }
